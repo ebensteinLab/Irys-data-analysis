1b676907e4e485961a14417c49a35e8a9b3c744e
hilasharim <hila.sharim@gmail.com>
2019-05-20 09:06:08 +0300

    Fix out of memory exception
    Stream molecules so q_cmap does not need to be saved in memory.
    Molecule regions are now outputted correctly without needing to run the script twice.

--- xmap_cmap_to_BED_stretched.py
+++ xmap_cmap_to_BED_stretched.py
@@ -26,11 +26,11 @@ r_cmap_positions = {} #dict of positions where key=chrom and val=list of tuples 
 correct_positions = {} #dict of correct genomic positions where key=chrom and val = positions list
 molRegions = {} #dict of stretched mol regions. key=ID, val=tup(start,end)
 
-def parse_key_file(key_file_path):
+def parseKeyFile(key_file_path):
     '''parse the chromosome key file created when digesting a fasta reference file into a cmap''' 
     chrom_lengths = {}
     chrom_names = {}
-    with open(args.key) as key_file:
+    with open(key_file_path) as key_file:
         for line in key_file:
             if not line.startswith("#") and not line.startswith("C"):
                 chr_id, chr_name, chr_len=line.strip().split()
@@ -48,34 +48,6 @@ def parseXmap (xmap_input_path, min_confidence):
                 if float(info[8]) >= min_confidence:
                     ID = info[1]
                     xmap_infos[ID] = info
-                    
-def findQCmapPositions (cmap_input_path, label_channel, posDict):
-    '''read label position information for all molecules that aligned to the reference'''
-    molecule_counter = 0
-    with open(cmap_input_path) as input_file:
-        prevID = ""
-        for line in input_file:
-            if line[0] != "#":
-                line = line.strip()
-                info = line.split()
-                ID = info[0]
-                if ID in xmap_infos: #if molecule aligned
-                    if ID != prevID: #check if new molecule is starting
-                        counter = 0
-                        molecule_counter += 1
-                        if molecule_counter % 1000 == 0:
-                            print(molecule_counter)
-                    if int(info[4]) == label_channel: #if label channel is the one we are querying
-                        pos = float(info[5])
-                        counter+=1
-                        siteID = str(counter)
-                        tup = (pos,siteID)
-                        if ID in posDict:
-                            posDict[ID].append(tup)
-                        else:
-                            posDict[ID] = [tup]
-                prevID = ID
-                    
 
 def findRCmapPositions (cmap_input_path, label_channel, posDict):
     '''read reference label positions from r_cmap'''
@@ -93,113 +65,135 @@ def findRCmapPositions (cmap_input_path, label_channel, posDict):
                         posDict[ID] = [float(pos)]
 
 
-def correctMoleculeLabelPositions (molID, refID, chrom_lengths, chrom_names):
-    #make the connection between the molecule label to the reference label
-    alignment = xmap_infos[molID][13]
-    matches = alignment[1:len(alignment)-1].split(")(")
+def skipHeader(file_handle):
+    line = file_handle.readline()
+    while line.startswith("#"):
+        line = file_handle.readline()
+    return line
+
+def addLabel(labels_dict, label_info):
+    label_pos = float(label_info[5])
+    label_channel = int(label_info[4])
+    if label_channel != 0:
+        labels_dict[label_channel].append(label_pos)
+    
+
+def getNextMoleculeLabels(file, line):
+    labels_dict = {1:[], 2:[]}
+    mol_id = line.split()[0]
+    while line and line.split()[0] == mol_id:
+        label_info = line.strip().split()
+        addLabel(labels_dict, label_info)
+        line = file.readline()
+    return (labels_dict,line)
+
+def parseAlignmentString(alignment_string):
+    matches = alignment_string[1:len(alignment_string)-1].split(")(")
     tup_matches = []
     for match in matches:
         splitPos = match.find(",")
         tup = (match[:splitPos],match[splitPos+1:])
         tup_matches.append(tup)
+    return tup_matches
 
-    refPositions = []
-    queryPositions = []
+def getRefAndQueryPositions(labels_dict, alignment_label_channel, alignment_string_matches, mol_orientation, ref_id):
+    ref_positions = []
+    mol_positions = []
+    for tup in alignment_string_matches:
+        ref_pos = r_cmap_positions[ref_id][int(tup[0])-1]
+        mol_pos = labels_dict[alignment_label_channel][int(tup[1])-1]
+        if mol_pos not in mol_positions:
+            ref_positions.append(ref_pos)
+            mol_positions.append(mol_pos)
+
+    if mol_orientation == "-":
+        ref_positions = ref_positions[::-1]
+        mol_positions = mol_positions[::-1]
+
+    return (mol_positions, ref_positions)
+
+def getStretchedLabelPositions(ius, labels_dict, query_label_channel):
+    corrected_positions = []
+    for label in labels_dict[query_label_channel]:
+        corrected_positions.append(ius(label))
+    return corrected_positions
+
+def getMolRegion(corrected_query_labels, corrected_alignment_labels):
+    if len(corrected_query_labels) == 0:
+        mol_start_pos = min(corrected_alignment_labels)
+        mol_end_pos = max(corrected_alignment_labels)
+    else:
+        mol_start_pos = min(min(corrected_query_labels), min(corrected_alignment_labels))
+        mol_end_pos = max(max(corrected_query_labels), max(corrected_alignment_labels))
+    return (mol_start_pos, mol_end_pos)
+
+def writeMolLabelsToFile(mol_label_list, ref_id, padding, output_file_handle):
+    chrom_name = chrom_names[ref_id]
+    for label in mol_label_list:
+        start_pos = int(label) - padding
+        end_pos = int(label) + padding
+        if start_pos < 0:
+            start_pos = 0
+        if end_pos > chrom_lengths[chrom_name]:
+            end_pos = chrom_lengths[chrom_name]
+        line = "\t".join([chrom_name, str(start_pos), str(end_pos)]) + "\n"
+        output_file_handle.write(line)
+
+def writeMolRegionToFile(corrected_query_labels, corrected_alignment_labels, mol_id, ref_id, padding, output_file_handle):
+    mol_start_pos, mol_end_pos = getMolRegion(corrected_query_labels, corrected_alignment_labels)
+    chrom_name = chrom_names[ref_id]
+    mol_start_pos = int(mol_start_pos) - padding
+    mol_end_pos = int(mol_end_pos) + padding
+    if mol_start_pos < 0:
+        mol_start_pos = 0
+    if mol_end_pos > chrom_lengths[chrom_name]:
+        mol_end_pos = chrom_lengths[chrom_name]
+    line = "\t".join([chrom_name, str(mol_start_pos), str(mol_end_pos), mol_id, "0", "+"]) + "\n"
+    output_file_handle.write(line)
     
-    for tup in tup_matches:
-        refPos = r_cmap_positions[refID][int(tup[0])-1]
-        found = False
-        i = 0
-        while found == False and i<len(alignment_q_cmap_positions[molID]):
-            if alignment_q_cmap_positions[molID][i][1] == tup[1]:
-                queryPos = alignment_q_cmap_positions[molID][i][0]
-                found = True
-            else:
-                i += 1
-
-        if found == True:
-            if xmap_infos[molID][7] == "+":
-                if queryPos not in queryPositions and refPos not in refPositions:
-                    refPositions.append(refPos)
-                    queryPositions.append(queryPos)
-            else:
-                if queryPos not in queryPositions and refPos not in refPositions:
-                    refPositions.insert(0,refPos)
-                    queryPositions.insert(0,queryPos)
-
-    ius = interpolate.InterpolatedUnivariateSpline(queryPositions,refPositions, k=1)
-
-    min_pos = min(refPositions)
-    max_pos = max(refPositions)
-    
-    for pos in query_q_cmap_positions[molID]:
-        correctPos = ius(pos[0])
-        chrom_name = chrom_names[refID]
-        if correctPos >= 0 and correctPos <= chrom_lengths[chrom_name]:
-            if refID in correct_positions:
-                correct_positions[refID].append(correctPos)
-            else:
-                correct_positions[refID] = [correctPos]
-
-            if correctPos > max_pos:
-                max_pos = correctPos
-            elif correctPos < min_pos:
-                min_pos = correctPos
-
-    tup = (chrom_name,min_pos,max_pos)
-    molRegions[molID] = tup
 
 
-def correctAllMoleculePositions (chrom_lengths, chrom_names):
-    mol_counter = 0
-    for ID in xmap_infos:
-        if ID in query_q_cmap_positions:
-            refID = xmap_infos[ID][2]
-            correctMoleculeLabelPositions(ID, refID, chrom_lengths, chrom_names)
-            mol_counter += 1
-            if mol_counter % 1000 == 0:
-                print(mol_counter)
+def getMoleculeStretchedAlignments(q_cmap_input_path, alignment_label_channel, query_label_channel, labels_output_path, molecules_output_path):
+    counter = 0
+    with open(q_cmap_input_path) as q_cmap_file:
+        line = skipHeader(q_cmap_file)
+        while line:
+            mol_id = line.split()[0]
+            labels_dict, line = getNextMoleculeLabels(q_cmap_file,line)
+            if mol_id not in xmap_infos:
+                continue
+            counter += 1
+            if counter % 1000 == 0:
+                print(counter)
+            alignment_string_matches = parseAlignmentString(xmap_infos[mol_id][13])
+            mol_positions, ref_positions = getRefAndQueryPositions(labels_dict, alignment_label_channel, alignment_string_matches, xmap_infos[mol_id][7], xmap_infos[mol_id][2])
 
+            ius = interpolate.InterpolatedUnivariateSpline(mol_positions,ref_positions, k=1)
+            corrected_query_labels = getStretchedLabelPositions(ius, labels_dict, query_label_channel)
+            corrected_alignment_labels = getStretchedLabelPositions(ius, labels_dict, alignment_label_channel)
 
-def createBED (output_file_path, chrom_names):
-    with open(output_file_path, 'w') as output_file:
-        for ref in correct_positions:
-            chrom_name = chrom_names[ref]
-            for pos in correct_positions[ref]:
-                start = int(pos)-1
-                end=int(pos)+2
-                lineToPrint = chrom_name+"\t"+str(start)+"\t"+str(end)+"\n"
-                output_file.write(lineToPrint)
+            if labels_output_path != "":
+                with open(labels_output_path, 'a') as labels_output_file:
+                    writeMolLabelsToFile(corrected_query_labels, xmap_infos[mol_id][2], 1, labels_output_file)
 
+            if molecules_output_path != "":
+                with open(molecules_output_path, 'a') as molecules_output_file:
+                    writeMolRegionToFile(corrected_query_labels, corrected_alignment_labels, mol_id, xmap_infos[mol_id][2], 10, molecules_output_file)
+                    
 
-def printMolRegionsBed(output_file_path):
-    with open(output_file_path, 'w') as output_file:
-        for ID,tup in molRegions.items():
-            lineToPrint = tup[0]+"\t"+str(int(tup[1])-10)+"\t"+str(int(tup[2])+10)+"\t"+ID+"\t"+str(0)+"\t+\n"
-            output_file.write(lineToPrint)
 
 
 if not args.label_bed and not args.molecule_bed:
-    print("Missing Argument. At least one of -lb and -mb must be selected.")
-    sys.exit(1)            
-            
-chrom_lengths, chrom_names = parse_key_file(args.key)
+    print("Missing Argument. At least one of -lb and -mb should be selected.")
+    sys.exit(1)
+    
+
+chrom_lengths, chrom_names = parseKeyFile(args.key)
 parseXmap(args.xmap, args.min_confidence)
 print("done parsing xmap")
 
-if args.alignment_label_channel != args.query_label_channel:
-    findQCmapPositions(args.q_cmap, args.alignment_label_channel, alignment_q_cmap_positions)
-    findQCmapPositions(args.q_cmap, args.query_label_channel, query_q_cmap_positions)
-else:
-    findQCmapPositions(args.q_cmap, args.alignment_label_channel, alignment_q_cmap_positions)
-    query_q_cmap_positions = alignment_q_cmap_positions.copy()
-print("done finding q_cmap positions")
-
 findRCmapPositions(args.r_cmap, 1, r_cmap_positions)
 print("done finding r_cmap_positions")
-
-correctAllMoleculePositions(chrom_lengths, chrom_names)
-print("done calculating genomic positions")
 
 if args.output_dir == None:
     output_dir = os.getcwd()
@@ -209,13 +203,15 @@ else:
 if args.label_bed:
     labels_file_name = args.prefix+"conf."+str(args.min_confidence)+".labels_channel."+str(args.query_label_channel)+".bed"
     labels_output_path = os.path.join(output_dir,labels_file_name)
-    createBED(labels_output_path, chrom_names)
-    print("done writing BED file")
+else:
+    labels_output_path = ""
 
 if args.molecule_bed:
-    molecules_file_name = args.prefix+"conf."+str(args.min_confidence)+".molecules_channel."+str(args.query_label_channel)+".bed"
+    molecules_file_name = args.prefix+"conf."+str(args.min_confidence)+".molecules.bed"
     molecule_output_path = os.path.join(output_dir,molecules_file_name)
-    printMolRegionsBed(molecule_output_path)
-    print("done writing mol regions BED")
+else:
+    molecule_output_path = ""
+
+getMoleculeStretchedAlignments(args.q_cmap, args.alignment_label_channel, args.query_label_channel, labels_output_path, molecule_output_path)
 
 print("analysis complete")
